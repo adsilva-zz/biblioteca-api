9dccadfa770a00ea012f3feee83bd9b97e3ec9dc
Amanda da Silva Ito <adsilva32@gmail.com>
2019-05-22 12:43:50 -0300

    Ajuste de  Teste

--- src/test/java/com/sistemalerlivros/api/controller/AutorControllerTest.java
+++ src/test/java/com/sistemalerlivros/api/controller/AutorControllerTest.java
@@ -1,29 +1,40 @@
 package com.sistemalerlivros.api.controller;
 
+import java.time.LocalDate;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sistemalerlivros.api.dto.AutorDTO;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @RunWith(SpringRunner.class)
-@WebMvcTest(AutorController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AutorControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
 
+	@Autowired
+	private ObjectMapper mapper;
+
 	@Test
 	public void cadastrarAutorComSucesso() throws Exception {
-		 mvc.perform( MockMvcRequestBuilders
-			      .post("/autores")
-			      .content(asJsonString(new AutorDTO("nome", )))
-			      .contentType(MediaType.APPLICATION_JSON)
-			      .accept(MediaType.APPLICATION_JSON))
-			      .andExpect(status().isCreated())
-			      .andExpect(MockMvcResultMatchers.jsonPath("$.employeeId").exists());
+		mvc.perform(MockMvcRequestBuilders.post("/api/v1/biblioteca/autores")
+				.content(mapper.writeValueAsString(new AutorDTO("nome", LocalDate.now(), null)))
+				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.idAutor").exists()).andExpect(status().isCreated());
 	}
+
 }
